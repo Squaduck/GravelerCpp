5d8bb34811d0c29e86272741a08ec9877a77117f
Drake Elwartowski <squaduck@gmail.com>
2024-08-24 00:31:08 -0400

    Adapt an old project to this and use similar optimizations to the C# version

---
+++ Graveler.cpp
@@ -0,0 +1,77 @@
+#include <iostream>
+#include "timestuff.h"
+#include <chrono>
+#include <thread>
+#include <random>
+#include <future>
+#include <bit>
+#include <bitset>
+
+void roll_rounds(long count, std::promise<uint8_t> &&promise)
+{
+    std::random_device rd; // secure, but slow random to seed faster random
+    std::default_random_engine randEngine(rd());
+    std::uniform_int_distribution<uint64_t> randDistribution(0,UINT64_MAX);
+
+    uint8_t localTotal;
+    uint8_t bestLocalTotal = 0;
+
+    for(int x = 0; x < count; x++)
+    {
+        localTotal = 0;
+        for (uint32_t i = 0; i < 231 / 64; i++)
+        {
+            localTotal += std::__popcount(randDistribution(randEngine) & randDistribution(randEngine));
+        }
+        localTotal += std::__popcount((randDistribution(randEngine) & randDistribution(randEngine)) & 0b111111111111111111111111111111111111111);
+        if (localTotal > bestLocalTotal)
+            bestLocalTotal = localTotal;
+    }
+
+    promise.set_value(bestLocalTotal);
+}
+
+uint8_t multithreadRoll_rounds(uint32_t count)
+{
+    unsigned int threadCount = std::thread::hardware_concurrency() >= count ? count : std::thread::hardware_concurrency(); // Only use all threads if there are more than the count
+
+    std::thread threads[threadCount];
+    std::promise<uint8_t> promises[threadCount];
+    std::future<uint8_t> futures[threadCount];
+
+    uint32_t leftover = count % threadCount;
+    uint32_t baseThreadCount = (count - leftover) / threadCount;
+
+    // eg count 13 threads 5, base would be 2
+    // there will be three left over that should be given to first three threads
+
+    for (uint32_t i = 0; i < threadCount; i++)
+    {
+        futures[i] = promises[i].get_future();
+
+        if (i >= leftover)
+            threads[i] = std::thread(roll_rounds, baseThreadCount, std::move(promises[i]));
+        else
+            threads[i] = std::thread(roll_rounds, baseThreadCount + 1, std::move(promises[i]));
+    }
+
+    uint8_t bestTotal = 0;
+
+    for (uint32_t i = 0; i < threadCount; i++)
+    {
+        threads[i].join();
+        uint8_t val = futures[i].get();
+        if (val > bestTotal)
+            bestTotal = val;
+    }
+
+    return bestTotal;
+}
+
+int main(int argc, char *argv[])
+{
+    auto startTime = std::chrono::steady_clock::now();
+    uint8_t result = multithreadRoll_rounds(100000000);
+    auto elapsedTime = since(startTime);
+    printf("Highest number of 1s rolled in %d rounds: %d\nRan in %s\n", 100000000, result, humanFriendlyTime(elapsedTime.count()).c_str());
+}
---
+++ timestuff.h
@@ -0,0 +1,173 @@
+#pragma once
+
+#include <stdlib.h>
+#include <chrono>
+#include <string>
+
+template <
+    class result_t = std::chrono::microseconds,
+    class clock_t = std::chrono::steady_clock,
+    class duration_t = std::chrono::microseconds>
+
+auto since(std::chrono::time_point<clock_t, duration_t> const &start)
+{
+    return std::chrono::duration_cast<result_t>(clock_t::now() - start);
+}
+
+const long long monthLengthInMicroseconds = 2629746000000;
+// 30.436875 days
+
+// takes a long long, and returns something like "1 months, 5 days, 17 hours, 28 minutes, 34 seconds, 690 miliseconds, and 125 microseconds"
+// accurate for longer times, but may have weirdness around 1 mo, because the month counter assumes a month is ~30.44 days, but the day counter assumes it's 31.
+std::string humanFriendlyLongTime(int64_t time)
+{
+    int64_t timeSpan = time;
+
+    std::string result("");
+
+    char number[10];
+    sprintf(number, "%ld", timeSpan % 1000);
+    result.append(number);
+    result.append(" microseconds");
+
+    if (timeSpan < 1000)
+        return result;
+
+    timeSpan = (timeSpan - (timeSpan % 1000)) / 1000;
+
+    result.insert(0, " miliseconds, and ");
+    sprintf(number, "%ld", timeSpan % 1000);
+    result.insert(0, number);
+
+    if (timeSpan < 1000)
+        return result;
+
+    timeSpan = (timeSpan - (timeSpan % 1000)) / 1000;
+
+    result.insert(0, " seconds, ");
+    sprintf(number, "%ld", timeSpan % 60);
+    result.insert(0, number);
+
+    if (timeSpan < 60)
+        return result;
+
+    timeSpan = (timeSpan - (timeSpan % 60)) / 60;
+
+    result.insert(0, " minutes, ");
+    sprintf(number, "%ld", timeSpan % 60);
+    result.insert(0, number);
+
+    if (timeSpan < 60)
+        return result;
+
+    timeSpan = (timeSpan - (timeSpan % 60)) / 60;
+
+    result.insert(0, " hours, ");
+    sprintf(number, "%ld", timeSpan % 24);
+    result.insert(0, number);
+
+    if (timeSpan < 24)
+        return result;
+
+    timeSpan = (timeSpan - (timeSpan % 24)) / 24;
+
+    result.insert(0, " days, ");
+    sprintf(number, "%ld", timeSpan % 31);
+    result.insert(0, number);
+
+    if (time < monthLengthInMicroseconds)
+        return result;
+
+    timeSpan = (time - (time % monthLengthInMicroseconds)) / monthLengthInMicroseconds;
+
+    result.insert(0, " months, ");
+    sprintf(number, "%ld", timeSpan % 12);
+    result.insert(0, number);
+
+    if (timeSpan < 12)
+        return result;
+
+    timeSpan = (timeSpan - (timeSpan % 12)) / 12;
+
+    result.insert(0, " years, ");
+    sprintf(number, "%ld", timeSpan);
+    result.insert(0, number);
+
+    return result;
+}
+
+// takes a long long, and returns something like "1 months, 5 days, 17 hours, 28 minutes, 34 seconds, 690 miliseconds, and 125 microseconds"
+// accurate for shorter times, but may drift at long lengths, because it doesn't account for leap years. Assumes a month is 30 days
+std::string humanFriendlyTime(int64_t timeSpan)
+{
+    std::string result("");
+
+    char number[10];
+    sprintf(number, "%ld", timeSpan % 1000);
+    result.append(number);
+    result.append(" microseconds");
+
+    if (timeSpan < 1000)
+        return result;
+
+    timeSpan = (timeSpan - (timeSpan % 1000)) / 1000;
+
+    result.insert(0, " miliseconds, and ");
+    sprintf(number, "%ld", timeSpan % 1000);
+    result.insert(0, number);
+
+    if (timeSpan < 1000)
+        return result;
+
+    timeSpan = (timeSpan - (timeSpan % 1000)) / 1000;
+
+    result.insert(0, " seconds, ");
+    sprintf(number, "%ld", timeSpan % 60);
+    result.insert(0, number);
+    if (timeSpan < 60)
+        return result;
+
+    timeSpan = (timeSpan - (timeSpan % 60)) / 60;
+
+    result.insert(0, " minutes, ");
+    sprintf(number, "%ld", timeSpan % 60);
+    result.insert(0, number);
+
+    if (timeSpan < 60)
+        return result;
+
+    timeSpan = (timeSpan - (timeSpan % 60)) / 60;
+
+    result.insert(0, " hours, ");
+    sprintf(number, "%ld", timeSpan % 24);
+    result.insert(0, number);
+
+    if (timeSpan < 24)
+        return result;
+
+    timeSpan = (timeSpan - (timeSpan % 24)) / 24;
+
+    result.insert(0, " days, ");
+    sprintf(number, "%ld", timeSpan % 30);
+    result.insert(0, number);
+
+    if (timeSpan < 30)
+        return result;
+
+    timeSpan = (timeSpan - (timeSpan % 30)) / 30;
+
+    result.insert(0, " months, ");
+    sprintf(number, "%ld", timeSpan % 12);
+    result.insert(0, number);
+
+    if (timeSpan < 12)
+        return result;
+
+    timeSpan = (timeSpan - (timeSpan % 12)) / 12;
+
+    result.insert(0, " years, ");
+    sprintf(number, "%ld", timeSpan);
+    result.insert(0, number);
+
+    return result;
+}
